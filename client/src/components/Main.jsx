e6bab516c34ab2b0a4e168bfbcfdc5c7263d9cad
AlexandrZ1 <alexzinchenk0o01@gmail.com>
2021-07-09 14:04:32 +0300

    add Css files

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,9 +2,10 @@ import Input from './Input'
 import Sort from './sort/Sort'
 import List from './todo/List'
 import Pagination from './Pagination'
+import style from './Main.module.css'
 const Main = () => {
     return (
-        <div className="main">
+        <div className={style}>
             <h1>ToDo</h1>
             <Input />
             <Sort />
