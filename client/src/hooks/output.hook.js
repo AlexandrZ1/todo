75fda0eebbeb1e984fa6174c3a53e089a9da7a82
AlexandrZ1 <sann12346464@gmail.com>
2021-07-15 17:08:07 +0300

    Fix coding style in sortTodos function

--- client/src/hooks/output.hook.js
+++ client/src/hooks/output.hook.js
@@ -1,5 +1,5 @@
 export const useOutput = (idSelect, typeSort, array) => {
-  const selectTodos = () => {
+  const selectTodos = (array) => {
     if (idSelect === 1) return array.filter((_) => true);
     if (idSelect === 2) return array.filter((item) => item.done);
     if (idSelect === 3) return array.filter((item) => !item.done);
@@ -10,6 +10,6 @@ export const useOutput = (idSelect, typeSort, array) => {
       ? array.sort((a, b) => (a.date < b.date ? 1 : -1))
       : array.sort((a, b) => (a.date > b.date ? 1 : -1));
 
-  const resTodos = sortTodos(selectTodos());
+  const resTodos = sortTodos(selectTodos(array));
   return { resTodos };
 };
